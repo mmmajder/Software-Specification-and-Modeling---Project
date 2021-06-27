bfc9723580aef7aa4f4a44aefc5b67adeabd713f
MAJDER <ajder.milan2000@gmail.com>
2021-06-27 18:48:03 +0200

    Added additionOfContributor.fxml, createContributor.fxml, memberCRUD.fxml, membership.fxml, reservations.fxml

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -22,7 +22,7 @@ public class LoginController {
 
     @FXML
     private void switchToUser(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/bookCRUD.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/createContributor.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);

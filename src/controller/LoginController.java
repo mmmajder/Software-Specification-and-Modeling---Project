43d77ea102455a01035ecd5333edf6b8db97b059
Aksont <aastanivuk@gmail.com>
2021-06-28 11:18:10 +0200

    LoginController and utils added

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import model.Account;
+import utils.StringUtils;
+import utils.exceptions.MissingValueException;
+
 public class LoginController {
+
+    public void login(String username, String password) throws MissingValueException {
+        if (StringUtils.isNullOrEmpty(username)) {
+            throw new MissingValueException("username");
+        }
+        else if (StringUtils.isNullOrEmpty(password)) {
+            throw new MissingValueException("password");
+        }
+
+        //TODO ask for account from repo
+    }
 }

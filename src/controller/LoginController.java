cca842daa4c40a2789fd9c10c8ff43dc0d446635
Aksont <aastanivuk@gmail.com>
2021-06-28 17:36:59 +0200

    LoginController reorganized

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,22 +2,35 @@ package controller;
 
 import model.Account;
 import model.Library;
+import services.AccountService;
 import services.LibraryService;
 import utils.StringUtils;
+import utils.exceptions.InvalidAccountPassword;
 import utils.exceptions.MissingValueException;
+import utils.exceptions.NoAccountWithThatUsername;
+
+import java.util.ArrayList;
 
 public class LoginController {
 
-    static public void login(String username, String password) throws MissingValueException {
-        //TODO Library l
+    private ArrayList<Account> accounts;
+
+    public LoginController(ArrayList<Account> accounts){
+        this.accounts = accounts;
+    }
+
+    public void login(String username, String password) throws MissingValueException, NoAccountWithThatUsername, InvalidAccountPassword {
+        validateInputValues(username, password);
+        Account a = AccountService.getAccount(accounts, username, password);
+        //TODO what should be done with the Account a?
+    }
+
+    private void validateInputValues(String username, String password) throws MissingValueException {
         if (StringUtils.isNullOrEmpty(username)) {
             throw new MissingValueException("username");
         }
         if (StringUtils.isNullOrEmpty(password)) {
             throw new MissingValueException("password");
         }
-
-        //Account a = LibraryService.getAccount(l, username, password);
-        //TODO what should be done with the Account a?
     }
 }

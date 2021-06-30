f7eaeb375ff660edb08f31584d701524c92395e7
Aksont <aastanivuk@gmail.com>
2021-06-30 13:29:33 +0200

    MembershipController implemented

---
+++ src/controller/MembershipControler.java
@@ -0,0 +1,37 @@
+package controller;
+
+import model.Library;
+import model.Member;
+import model.Payment;
+import model.enums.MemberType;
+import utils.exceptions.InvalidTransactionException;
+
+import java.time.LocalDate;
+
+public class MembershipControler {
+
+    private Library library;
+
+    public MembershipControler(Library library){ this.library = library; }
+
+    public void payMembership(Member m, int numOfMonths) throws InvalidTransactionException {
+        MemberType  type = m.getType();
+        double price = library.getCurrentCatalog().getPrice(type);
+
+        if (contactBankingSystem()) {
+            createPayment(m, numOfMonths);
+            m.prolongSubscription();
+        } else{
+            throw new InvalidTransactionException();
+        }
+    }
+
+    private void createPayment(Member m, int numOfMonths){
+        //TODO nextId value
+        int nextId = 0;
+        Payment newPayment = new Payment(nextId, LocalDate.now().plusMonths(numOfMonths), m);
+        m.addPayment(newPayment);
+    }
+
+    private boolean contactBankingSystem(){ return true;}
+}

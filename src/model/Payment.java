b8c46f5d1212b3be133fc3bcf9af47c95d150104
Aksont <aastanivuk@gmail.com>
2021-07-03 16:52:27 +0200

    added numOfMonths to Payment, needed for reports

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -7,13 +7,29 @@ public class Payment {
     private LocalDate paymentDate;
     private LocalDate validToDate;
     private Member member;
+    private int numOfMonths;
 
-    public Payment(int paymentId, LocalDate paymentDate, LocalDate validToDate, Member member){
+    public Payment(int paymentId, LocalDate paymentDate, LocalDate validToDate, Member member, int numOfMonths){
         this.paymentId = paymentId;
         this.paymentDate = paymentDate;
         this.validToDate = validToDate;
         this.member = member;
+        this.numOfMonths = numOfMonths;
     }
 
     public LocalDate getValidToDate() { return validToDate; }
+
+    public int getPaymentId() { return paymentId; }
+
+    public void setPaymentId(int paymentId) { this.paymentId = paymentId; }
+
+    public LocalDate getPaymentDate() { return paymentDate; }
+
+    public void setPaymentDate(LocalDate paymentDate) { this.paymentDate = paymentDate; }
+
+    public void setValidToDate(LocalDate validToDate) { this.validToDate = validToDate; }
+
+    public Member getMember() { return member; }
+
+    public void setMember(Member member) { this.member = member; }
 }

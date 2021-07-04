b625ba83d9ad96399ace19c1849e24cf3ef4c5da
andjmiskovic <andjelamiskovic2000@gmail.com>
2021-07-04 20:00:56 +0200

    added admin settings

--- src/controller/LibrarianReservationController.java
+++ src/controller/LibrarianReservationController.java
@@ -7,7 +7,9 @@ public class LibrarianReservationController {
 
     private Library library;
 
-    public LibrarianReservationController(Library library){ this.library = library; }
+    public LibrarianReservationController(Library library) {
+        this.library = library;
+    }
 
     public void acceptReservation(PendingReservation pr, Book book) throws NoSuchPendingRequestException {
         Reservation rb = createReservation(pr.getMember(), book);
@@ -16,7 +18,7 @@ public class LibrarianReservationController {
         //TODO notify the member
     }
 
-    private Reservation createReservation(Member member, Book book){
+    private Reservation createReservation(Member member, Book book) {
         //TODO new id value
         int id = 0;
         return new Reservation(id, member, book);
@@ -27,7 +29,7 @@ public class LibrarianReservationController {
         //TODO notify the member
     }
 
-    private void setReservation(Member member, Reservation reservation){
+    private void setReservation(Member member, Reservation reservation) {
         member.setReservation(reservation);
         library.addReservation(reservation);
     }
@@ -41,9 +43,9 @@ public class LibrarianReservationController {
 
         boolean pendingReservationExists = false;
 
-        for (PendingReservation pr : library.getPendingReservations()){
+        for (PendingReservation pr : library.getPendingReservations()) {
 
-            if (pr.getId() == prId){
+            if (pr.getId() == prId) {
 
                 library.removePendingReservation(pr);
                 pendingReservationExists = true;
--- src/controller/Reports.java
+++ src/controller/Reports.java
@@ -31,11 +31,11 @@ public class Reports {
         generateFile(lines, generateName());
     }
 
-    private void generateMembershipsPart(List<String> lines){
+    private void generateMembershipsPart(List<String> lines) {
         List<Payment> todaysPayments = getPayments(LocalDate.now());
         double totalEarnings = 0;
 
-        for (MemberType memberType : MemberType.values()){
+        for (MemberType memberType : MemberType.values()) {
             List<Payment> typePayments = getTypePayments(todaysPayments, memberType);
             totalEarnings += generateNumOfMonthsPaymentLine(lines, typePayments, memberType, 6);
             totalEarnings += generateNumOfMonthsPaymentLine(lines, typePayments, memberType, 12);
@@ -44,96 +44,96 @@ public class Reports {
         lines.add("Total earnings are: " + totalEarnings + ".");
     }
 
-    private List<Payment> getPayments(LocalDate date){
+    private List<Payment> getPayments(LocalDate date) {
         return library.getPayments().stream()
                 .filter(payment -> payment.getPaymentDate().isEqual(date))
                 .collect(Collectors.toList());
     }
 
-    private List<Payment> getTypePayments(List<Payment> payments, MemberType memberType){
+    private List<Payment> getTypePayments(List<Payment> payments, MemberType memberType) {
         return payments.stream()
                 .filter(payment -> payment.getMember().getType() == memberType)
                 .collect(Collectors.toList());
     }
 
-    private List<Payment> getNumOfMonthsPayments(List<Payment> payments, int numOfMonths){
+    private List<Payment> getNumOfMonthsPayments(List<Payment> payments, int numOfMonths) {
         return payments.stream()
                 .filter(payment -> payment.getNumOfMonths() == numOfMonths)
                 .collect(Collectors.toList());
     }
 
-    private double generateNumOfMonthsPaymentLine(List<String> lines, List<Payment> payments, MemberType memberType, int numOfMonths){
+    private double generateNumOfMonthsPaymentLine(List<String> lines, List<Payment> payments, MemberType memberType, int numOfMonths) {
         List<Payment> numOfMonthsPayments = getNumOfMonthsPayments(payments, numOfMonths);
         double price = numOfMonths == 6 ? library.getHalfAYearPrice(memberType) : library.getFullYearPrice(memberType);
         int numOfPayments = payments.size();
-        double earnings = price*numOfPayments;
+        double earnings = price * numOfPayments;
         lines.add(generateTypePaymentLine(memberType, numOfPayments, numOfMonths, earnings));
 
         return earnings;
     }
 
-    private String generateTypePaymentLine(MemberType memberType, int numOfPayments, int numOfMonths, double earnings){
+    private String generateTypePaymentLine(MemberType memberType, int numOfPayments, int numOfMonths, double earnings) {
         return numOfPayments + " memberships of type " + memberType + "for a period of " + numOfMonths + " were sold and made earnings of " + earnings + ".";
     }
 
-    private void generateIssuesPart(List<String> lines){
+    private void generateIssuesPart(List<String> lines) {
         List<IssuedBook> issuedBooks = getTodaysIssues();
         Set<Librarian> librariansThatIssued = getLibrariansThatIssued(issuedBooks);
 
-        for (Librarian librarian : librariansThatIssued){
+        for (Librarian librarian : librariansThatIssued) {
             generateLibrarianLines(lines, issuedBooks, librarian);
         }
     }
 
-    private void generateLibrarianLines(List<String> lines, List<IssuedBook> issuedBooks, Librarian librarian){
+    private void generateLibrarianLines(List<String> lines, List<IssuedBook> issuedBooks, Librarian librarian) {
         List<IssuedBook> librarianIssues = getLibrariansIssues(issuedBooks, librarian);
         int numOfIssues = librarianIssues.size();
         lines.add(generateNumOfIssuesLine(librarian, numOfIssues));
 
-        for (IssuedBook issuedBook : librarianIssues){
+        for (IssuedBook issuedBook : librarianIssues) {
             lines.add(generateIssuedBookLine(issuedBook));
         }
     }
 
-    private List<IssuedBook> getTodaysIssues(){
+    private List<IssuedBook> getTodaysIssues() {
         LocalDate todayDate = LocalDate.now();
         return library.getCurrentlyIssued().stream()
                 .filter(issuedBook -> issuedBook.getIssueDate().isEqual(todayDate))
                 .collect(Collectors.toList());
     }
 
-    private Set<Librarian> getLibrariansThatIssued(List<IssuedBook> issuedBooks){
+    private Set<Librarian> getLibrariansThatIssued(List<IssuedBook> issuedBooks) {
         return issuedBooks.stream()
                 .map(issuedBook -> issuedBook.getLibrarian())
                 .collect(Collectors.toSet());
     }
 
-    private List<IssuedBook> getLibrariansIssues(List<IssuedBook> issuedBooks, Librarian librarian){
+    private List<IssuedBook> getLibrariansIssues(List<IssuedBook> issuedBooks, Librarian librarian) {
         return issuedBooks.stream()
                 .filter(issuedBook -> issuedBook.getLibrarian().equals(librarian))
                 .collect(Collectors.toList());
     }
 
-    private String generateNumOfIssuesLine(Librarian librarian, int numOfIssues){
+    private String generateNumOfIssuesLine(Librarian librarian, int numOfIssues) {
         return getLibrarianNamingInfo(librarian) + " issued " + numOfIssues + " books.";
     }
 
-    private String getLibrarianNamingInfo(Librarian librarian){
+    private String getLibrarianNamingInfo(Librarian librarian) {
         return "Librarian " + librarian.getName() + " " + librarian.getSurname() + " (" + librarian.getAccount().getUsername() + ")";
     }
 
-    private String generateIssuedBookLine(IssuedBook issuedBook){
+    private String generateIssuedBookLine(IssuedBook issuedBook) {
         return getBookNamingInfo(issuedBook) + " to member " + getMemberNamingInfo(issuedBook.getMember());
     }
 
-    private String getBookNamingInfo(IssuedBook issuedBook){
+    private String getBookNamingInfo(IssuedBook issuedBook) {
         return issuedBook.getBook().getEdition().getTitle();
     }
 
-    private String getMemberNamingInfo(Member member){
+    private String getMemberNamingInfo(Member member) {
         String info = member.getName() + " " + member.getSurname();
 
-        if (member.getAccount() != null){
+        if (member.getAccount() != null) {
             info += " (" + member.getAccount().getUsername() + ")";
         }
 
@@ -146,16 +146,18 @@ public class Reports {
 
     private void generateEditionIssuesReport(LocalDate fromDate, LocalDate toDate, Integer n) throws IOException {
         List<EditionIssues> editionsIssues = getEditionsIssues(fromDate, toDate);
-        if (n == null || n > editionsIssues.size()) { n = editionsIssues.size(); }
+        if (n == null || n > editionsIssues.size()) {
+            n = editionsIssues.size();
+        }
         List<String> lines = generateLines(editionsIssues, fromDate, toDate, n);
         String filename = generateTitle(fromDate, toDate, n, n == editionsIssues.size());
         generateFile(lines, filename);
     }
 
-    private List<EditionIssues> getEditionsIssues(LocalDate fromDate, LocalDate toDate){
+    private List<EditionIssues> getEditionsIssues(LocalDate fromDate, LocalDate toDate) {
         Map<String, Integer> mappedEditionsIssues = new HashMap<>();
 
-        for (Book book : library.getBooks()){
+        for (Book book : library.getBooks()) {
             addNumOfBookIssues(mappedEditionsIssues, book, fromDate, toDate);
         }
 
@@ -165,7 +167,7 @@ public class Reports {
         return editionsIssues;
     }
 
-    private void addNumOfBookIssues(Map<String, Integer> mappedEditionsIssues, Book book, LocalDate fromDate, LocalDate toDate){
+    private void addNumOfBookIssues(Map<String, Integer> mappedEditionsIssues, Book book, LocalDate fromDate, LocalDate toDate) {
         String editionId = book.getEdition().getEditionId();
         int numOfIssues = getIssues(book, fromDate, toDate).size();
 
@@ -176,16 +178,16 @@ public class Reports {
         mappedEditionsIssues.put(editionId, numOfIssues);
     }
 
-    private List<IssuedBook> getIssues(Book book, LocalDate fromDate, LocalDate toDate){
+    private List<IssuedBook> getIssues(Book book, LocalDate fromDate, LocalDate toDate) {
         return book.getIssueHistory().stream()
                 .filter(issuedBook -> DateUtils.isDateWithin(issuedBook.getIssueDate(), fromDate, toDate))
                 .collect(Collectors.toList());
     }
 
-    private List<EditionIssues> transformIntoEditionIssues(Map<String, Integer> mappedEditionsIssues){
+    private List<EditionIssues> transformIntoEditionIssues(Map<String, Integer> mappedEditionsIssues) {
         List<EditionIssues> editionsIssues = new ArrayList<>();
 
-        for (String editionId : mappedEditionsIssues.keySet()){
+        for (String editionId : mappedEditionsIssues.keySet()) {
             EditionIssues editionIssues = new EditionIssues(editionId, mappedEditionsIssues.get(editionId));
             editionsIssues.add(editionIssues);
         }
@@ -193,17 +195,17 @@ public class Reports {
         return editionsIssues;
     }
 
-    private void sort(List<EditionIssues> editionsIssues){
+    private void sort(List<EditionIssues> editionsIssues) {
         EditionIssuesComparator comparator = new EditionIssuesComparator();
         editionsIssues.sort(comparator);
     }
 
-    private List<String> generateLines(List<EditionIssues> editionsIssues, LocalDate fromDate, LocalDate toDate, Integer n){
+    private List<String> generateLines(List<EditionIssues> editionsIssues, LocalDate fromDate, LocalDate toDate, Integer n) {
         List<String> lines = new ArrayList<>();
 
-        lines.add(generateTitle(fromDate, toDate, n,  n == editionsIssues.size()));
+        lines.add(generateTitle(fromDate, toDate, n, n == editionsIssues.size()));
 
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             lines.add(generateEditionIssuesLine(editionsIssues.get(i)));
         }
 
@@ -212,10 +214,10 @@ public class Reports {
         return lines;
     }
 
-    private String generateTitle(LocalDate fromDate, LocalDate toDate, Integer n, boolean allEditions){
+    private String generateTitle(LocalDate fromDate, LocalDate toDate, Integer n, boolean allEditions) {
         String line = "NUMBER OF ";
 
-        if (!allEditions){
+        if (!allEditions) {
             line += "TOP " + n;
         }
 
@@ -226,7 +228,7 @@ public class Reports {
         return line;
     }
 
-    private String generateEditionIssuesLine(EditionIssues editionIssues){
+    private String generateEditionIssuesLine(EditionIssues editionIssues) {
         Edition edition = library.getEdition(editionIssues.getEditionId());
 
         return "Edition " + edition.getTitle() + " by " + edition.getAuthor()
@@ -234,25 +236,25 @@ public class Reports {
                 + editionIssues.getNumOfIssues() + " times.";
     }
 
-    private String generateEditionsIssuesSumLine(List<EditionIssues> editionsIssues){
+    private String generateEditionsIssuesSumLine(List<EditionIssues> editionsIssues) {
         int numOfEditions = editionsIssues.size();
         int numOfIssuesSum = 0;
 
-        for (EditionIssues editionIssues : editionsIssues){
+        for (EditionIssues editionIssues : editionsIssues) {
             numOfIssuesSum += editionIssues.getNumOfIssues();
         }
 
         return "A total of " + numOfEditions + " editions were issued " + numOfIssuesSum + " times.";
     }
 
-    private String generateName(){
+    private String generateName() {
         return "Daily_" + getTodaysDateStr();
     }
 
-    private String generateName(LocalDate fromDate, LocalDate toDate, Integer n, boolean allEditions){
+    private String generateName(LocalDate fromDate, LocalDate toDate, Integer n, boolean allEditions) {
         String name = "";
 
-        if (allEditions){
+        if (allEditions) {
             name += "Issues_";
         } else {
             name += "Top" + n + "_Issues_";
@@ -264,11 +266,11 @@ public class Reports {
         return name;
     }
 
-    private String getTodaysDateStr(){
+    private String getTodaysDateStr() {
         return getDateStr(LocalDate.now());
     }
 
-    private String getDateStr(LocalDate date){
+    private String getDateStr(LocalDate date) {
         return StringUtils.dateToString(date, datePattern);
     }
 
@@ -276,7 +278,7 @@ public class Reports {
         Files.write(Paths.get(path + filename + extension), lines, StandardCharsets.UTF_8);
     }
 
-    public List<String> getReportTypes(){
+    public List<String> getReportTypes() {
         List<String> types = new ArrayList<>();
         types.add("Daily");
         types.add("Top10");

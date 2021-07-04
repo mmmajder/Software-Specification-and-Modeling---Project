2dc3a59b68066f712a1a64f795197673191007dc
Aksont <aastanivuk@gmail.com>
2021-07-04 03:15:16 +0200

    implemented EditionIssuesComparator

--- src/model/EditionIssues.java
+++ src/model/EditionIssues.java
@@ -1,2 +1,21 @@
-package model;public class EditionIssues {
+package model;
+
+import java.util.Comparator;
+
+public class EditionIssues {
+
+    private String editionId;
+    private int numOfIssues;
+
+    public EditionIssues(String editionId, int numOfIssues){
+        this.editionId = editionId;
+        this.numOfIssues = numOfIssues;
+    }
+
+    public String getEditionId() { return editionId; }
+
+    public int getNumOfIssues() { return numOfIssues; }
+
 }
+
+
--- src/model/EditionIssuesComparator.java
+++ src/model/EditionIssuesComparator.java
@@ -1,2 +1,11 @@
-package model;public class EditionIssuesComparator {
+package model;
+
+import java.util.Comparator;
+
+public class EditionIssuesComparator implements Comparator<EditionIssues> {
+
+    @Override
+    public int compare(EditionIssues firstEditionIssues, EditionIssues secondEditionIssues) {
+        return Integer.compare(firstEditionIssues.getNumOfIssues(), secondEditionIssues.getNumOfIssues());
+    }
 }

5478284d70874b0bc6f627241d0168fd67df586e
MAJDER <ajder.milan2000@gmail.com>
2021-07-04 14:16:22 +0200

    Added alerts for rent

--- src/view/librarian/BookRentController.java
+++ src/view/librarian/BookRentController.java
@@ -1,12 +1,17 @@
 package view.librarian;
 
 import controller.RentingController;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import model.Account;
 import model.Librarian;
 import model.Library;
+import utils.exceptions.BookNotFoundException;
+import utils.exceptions.BookRentingIsInvalidException;
+import utils.exceptions.MemberNotFoundException;
+import utils.exceptions.MemberUnableToRentException;
 
 public class BookRentController {
 
@@ -24,12 +29,28 @@ public class BookRentController {
         try {
             library = new Library(); // add
             RentingController rentingController = new RentingController(library);
-//            rentingController.rent(userIdLbl.getText(), sampleIdLbl.getText(), account.getPerson());
-        } catch (Exception e) {
-            // add exceptions
-            /*Alert a = new Alert(Alert.AlertType.WARNING);
-        a.setTitle("Alert");
-        a.setContentText("Use");*/
+            rentingController.rent(userIdLbl.getText(), sampleIdLbl.getText(), account.getPerson());
+        } catch (BookNotFoundException e) {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("There is no book with that ID");
+            a.show();
+        } catch (MemberNotFoundException e) {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("There is no member with that ID");
+            a.show();
+        } catch (BookRentingIsInvalidException e) {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("You can't rent that book");
+            a.show();
+        } catch (MemberUnableToRentException e) {
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("You have reached your maximum number of books");
+            a.show();
+            e.printStackTrace();
         }
     }
 }
